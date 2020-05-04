cf38ebde4e218ea920ec758d380ab33d606dc0c2
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-04 06:55:07 -0700

    Add messaging using local state hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = props => {
 
+  const [message, setMessage] = useState("");
+  const [history, setHistory] = useState([]);
+
+  function handleFormSubmission(event) {
+    event.preventDefault();
+    setMessage("");
+    setHistory([...history, event.target.message.value]);
+  }
+
+  return(
+    <React.Fragment>
+      <div className="chat">
+        {history.map(message => (
+          <p>{message}</p>
+        ))}
+      </div>
+      <form onSubmit={handleFormSubmission}>
+        <input type="text" name="message" value={message} onChange={event => setMessage(event.target.value)} />
+        <button>Send</button>
+      </form>
+    </React.Fragment>
+  )
 }
 
 export default App;
